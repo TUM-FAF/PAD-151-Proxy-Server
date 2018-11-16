88218c1a1a462a7ca325310834e1d9178a840c65
Alexei Sersun <alex.sersun@yahoo.com>
2018-11-16 23:47:50 +0200

    Add simple HTTP server to proxy and warehouse images.

---
+++ warehouse/app.rb
@@ -0,0 +1,17 @@
+require 'socket' # Provides TCPServer and TCPSocket classes
+
+puts "Running server on localhost:#{ENV['WAREHOUSE_PORT']}"
+
+server = TCPServer.new ENV['WAREHOUSE_PORT']
+
+while session = server.accept
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: text/html\r\n" # 2
+  session.print "\r\n" # 3
+  session.print "Hello world! The time is #{Time.now}" #4
+
+  session.close
+end
