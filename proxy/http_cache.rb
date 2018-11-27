ff863ae6dc45488a0d81f93269f0432ca9f57094
Alexei Sersun <alex.sersun@yahoo.com>
2018-11-27 14:21:14 +0200

    Implement basic cache.

--- proxy/http_cache.rb
+++ proxy/http_cache.rb
@@ -1,6 +1,18 @@
-require 'em-redis'
+require 'redis'
 
 module HttpCache
-  
+
+  def init_cache
+    @redis = Redis.new(host: 'redis', port: 6379)
+  end
+
+  def store_in_cache(key, value, expiry = 10)
+    @redis.set(key, value)
+    @redis.expire(key, expiry)
+  end
+
+  def try_restore_from_cache(key)
+    @redis.get(key)
+  end
 end
 
